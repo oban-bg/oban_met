f2678a73781fe7eab7686bdb359af8ef76582f1d
Parker Selbert <parker@sorentwo.com>
2022-11-29 09:31:25 -0600

    Prepare mixfile for package publication

--- mix.exs
+++ mix.exs
@@ -1,21 +1,35 @@
 defmodule Oban.Met.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :oban_met,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       aliases: aliases(),
+      package: package(),
       preferred_cli_env: [
         "ecto.gen.migration": :test,
         "test.ci": :test,
         "test.reset": :test,
         "test.setup": :test
       ],
+
+      # Description
+      name: "Oban Met",
+      description:
+        "A distributed, compacting, multidimensional, telemetry-powered time series datastore",
+
+      # Dialyzer
+      dialyzer: [
+        plt_core_path: "_build/#{Mix.env()}",
+        flags: [:error_handling, :underspecs]
+      ]
     ]
   end
 
@@ -30,8 +44,24 @@ defmodule Oban.Met.MixProject do
     ]
   end
 
+  def package do
+    [
+      organization: "oban",
+      files: ~w(lib/met.ex lib/oban mix.exs),
+      licenses: ["Commercial"],
+      links: []
+    ]
+  end
+
   def aliases do
     [
+      release: [
+        "cmd git tag v#{@version}",
+        "cmd git push",
+        "cmd git push --tags",
+        "hex.publish package --yes",
+        "lys.publish"
+      ],
       "test.reset": ["ecto.drop -r Oban.Met.Repo", "test.setup"],
       "test.setup": ["ecto.create -r Oban.Met.Repo --quiet", "ecto.migrate -r Oban.Met.Repo"],
       "test.ci": [
@@ -50,7 +80,10 @@ defmodule Oban.Met.MixProject do
       {:telemetry, "~> 1.1"},
       {:benchee, "~> 1.0", only: [:test, :dev], runtime: false},
       {:credo, "~> 1.6", only: [:test, :dev], runtime: false},
-      {:stream_data, "~> 0.5", only: [:test, :dev]}
+      {:dialyxir, "~> 1.0", only: [:test, :dev], runtime: false},
+      {:stream_data, "~> 0.5", only: [:test, :dev]},
+      {:lys_publish, "~> 0.1",
+       only: [:dev], optional: true, runtime: false, path: "../lys_publish"}
     ]
   end
 end
