df5548f57436c7ab1ac7cfabd194c9c5bb1158fc
Parker Selbert <parker@sorentwo.com>
2023-11-04 12:00:29 -0500

    Additional Oban.Met documentation

--- lib/met.ex
+++ lib/met.ex
@@ -1,6 +1,22 @@
 defmodule Oban.Met do
   @moduledoc """
   Metric introspection for Oban.
+
+  `Oban.Met` supervises a collection of autonomous modules for in-memory, distributed time-series
+  data with zero-configuration. `Oban.Web` relies on `Met` for queue gossip, detailed job counts,
+  and historic metrics.
+
+  ## Highlights
+
+  * Telemetry powered execution tracking for time-series data that is replicated between nodes,
+    filterable by label, arbitrarily mergeable over windows of time, and compacted for longer
+    playback.
+
+  * Centralized counting across queues and states with exponential backoff to minimize load and
+    data replication between nodes.
+
+  * Ephemeral data storage via data replication with handoff between nodes. All nodes have a
+    shared view of the cluster's data and new nodes are caught up when they come online.
   """
 
   use Supervisor
@@ -24,18 +40,18 @@ defmodule Oban.Met do
 
   @type latest_opts :: [
           filters: keyword(filter_value()),
-          group: String.t(),
-          lookback: pos_integer(),
-          operation: operation()
+          group: nil | label(),
+          lookback: pos_integer()
         ]
 
   @type timeslice_opts :: [
           by: pos_integer(),
           filters: keyword(filter_value()),
           group: nil | label(),
-          label: :any | String.t(),
+          label: nil | label(),
           lookback: pos_integer(),
-          operation: operation()
+          operation: operation(),
+          since: pos_integer()
         ]
 
   @doc false
@@ -49,7 +65,30 @@ defmodule Oban.Met do
     |> Map.put(:id, name)
   end
 
-  @doc false
+  @doc """
+  Start a Met supervisor for an Oban instance.
+
+  `Oban.Met` typically starts supervisors automatically when Oban instances initialize. However,
+  starting a supervisor manually can be used if `auto_start` is disabled.
+
+  ## Options
+
+  These options are required; without them the supervisor won't start:
+
+  * `:conf` — configuration for a running Oban instance, required
+
+  * `:name` — an optional name for the supervisor, defaults to `Oban.Met`
+
+  ## Example
+
+  Start a supervisor for the default Oban instance:
+
+      Oban.Met.start_link(conf: Oban.config())
+
+  Start a supervisor with a custom name:
+
+      Oban.Met.start_link(conf: Oban.config(), name: MyApp.MetSup)
+  """
   @spec start_link(Keyword.t()) :: Supervisor.on_start()
   def start_link(opts) when is_list(opts) do
     conf = Keyword.fetch!(opts, :conf)
@@ -62,6 +101,36 @@ defmodule Oban.Met do
   Retrieve stored producer checks.
 
   This mimics the output of the legacy `Oban.Web.Plugins.Stats.all_gossip/1` function.
+
+  Checks are queried approximately every second and broadcast to all connected nodes, so each node
+  is a replica of checks from the entire cluster. Checks are stored for 30 seconds before being
+  purged.
+
+  ## Output
+
+  Checks are the result of `Oban.check_queue/1`, and the exact contents depends on which
+  `Oban.Engine` is in use. A `Basic` engine check will look similar to this:
+
+      %{
+        uuid: "2dde4c0f-53b8-4f59-9a16-a9487454292d",
+        limit: 10,
+        node: "me@local",
+        paused: false,
+        queue: "default",
+        running: [100, 102],
+        started_at: ~D[2020-10-07 15:31:00],
+        updated_at: ~D[2020-10-07 15:31:00]
+      }
+
+  ## Examples
+
+  Get all current checks:
+
+      Oban.Met.checks()
+
+  Get current checks for a non-standard Oban isntance:
+
+      Oban.Met.checks(MyOban)
   """
   @spec checks(Oban.name()) :: [map()]
   def checks(oban \\ Oban) do
@@ -103,10 +172,51 @@ defmodule Oban.Met do
   end
 
   @doc """
-  Get the latest values for a series, optionally subdivided by a label.
+  Get the latest values for a gauge series, optionally subdivided by a label.
 
   Unlike queues and workers, states are static and constant, so they'll always show up in the
   counts or subdivision maps.
+
+  ## Gauge Series
+
+  Latest counts only apply to `Gauge` series. There are two gauges available (as reported by
+  `series/1`:
+
+  * `:exec_count` — jobs executing at that moment, including `node`, `queue`, `state`, and
+    `worker` labels.
+
+  * `:full_count` — jobs in the database, including `queue`, and `state` labels.
+
+  ## Examples
+
+  Get the `:full_count` value without any grouping:
+
+      Oban.Met.latest(:full_count)
+      %{"all" => 99}
+
+  Group the `:full_count` value by state:
+
+      Oban.Met.latest(:full_count, group: "state")
+      %{"available" => 9, "completed" => 80, "executing" => 5, ...
+
+  Group results by queue:
+
+      Oban.Met.latest(:exec_count, group: "queue")
+      %{"alpha" => 9, "gamma" => 3}
+
+  Group results by node:
+
+      Oban.Met.latest(:exec_count, group: "node")
+      %{"worker.1" => 6, "worker.2" => 5}
+
+  Filter values by node:
+
+      Oban.Met.latest(:exec_count, filters: [node: "worker.1"])
+      %{"all" => 6}
+
+  Filter values by queue and state:
+
+      Oban.Met.latest(:exec_count, filters: [node: "worker.1", "worker.2"])
   """
   @spec latest(Oban.name(), series()) :: counts() | sub_counts()
   def latest(oban \\ Oban, series, opts \\ []) do
@@ -142,7 +252,7 @@ defmodule Oban.Met do
 
   Retreive a 3 second timeslice of the `exec_time` sketch:
 
-      Oban.Met.timeslice(Oban, "exec_time", lookback: 3)
+      Oban.Met.timeslice(Oban, :exec_time, lookback: 3)
       [
         {2, 16771374649.128689, nil},
         {1, 24040058779.3428, nil},
@@ -151,7 +261,7 @@ defmodule Oban.Met do
 
   Group `exec_time` slices by the `queue` label:
 
-      Oban.Met.timeslice(Oban, "exec_time", group: "queue")
+      Oban.Met.timeslice(Oban, :exec_time, group: "queue")
       [
         {1, 9970235387.031698, "analysis"},
         {0, 11700429279.446463, "analysis"},
