3508840b52a4e6565ba4a4e5cc35fe11a4f0c948
Parker Selbert <parker@sorentwo.com>
2025-01-07 13:59:59 +0000

    Tweak assert and receive timing for ex_unit

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,4 @@
 Oban.Met.Repo.start_link()
 Oban.Met.LiteRepo.start_link()
-ExUnit.start()
+ExUnit.start(assert_receive_timeout: 500, refute_receive_timeout: 50, exclude: [:skip])
 Ecto.Adapters.SQL.Sandbox.mode(Oban.Met.Repo, :manual)
